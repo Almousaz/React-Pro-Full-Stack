88b2acc41056eca1b28bbcae3274ebcbfeb148d5
Ali Mousazada <mousazada.ali@gmail.com>
2025-01-06 18:35:29 -0500

    starting backend part with mode and schema data

--- LoginRegister/server.js
+++ LoginRegister/server.js
@@ -20,4 +20,9 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`));
+
+process.on("unhandledRejection", (err) => {
+    console.log(`An error occurred: ${err.message}`);
+    server.close(() => process.exit(1));
+  });
