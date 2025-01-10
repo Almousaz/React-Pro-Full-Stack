43de4fdca9ff3cc6ef8d5fdb5f51b66bbd170fe8
Ali Mousazada <mousazada.ali@gmail.com>
2025-01-10 14:43:07 -0500

    login protected route started with setup backend and connect to db

---
+++ SignLoginProtectRoute/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connectDB = require('./config/database');
+
+
+
+
+// connect to database
+connectDB();
+
+
+
+
+//  Middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+const PORT = process.env.PORT || 6060;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
