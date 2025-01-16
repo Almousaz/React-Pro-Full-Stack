8111e374c700478ca881e1747118b9a5d4c803d8
Ali Mousazada <mousazada.ali@gmail.com>
2025-01-16 14:48:19 -0500

    working on delete and get products on backend side

--- EcommerceWeb_CRUD/routes/Product.js
+++ EcommerceWeb_CRUD/routes/Product.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { ProductCreate , Update } from "../controllers/Product.js";
+import { DeleteProduct, GetProducts, ProductCreate , UpdateProduct } from "../controllers/Product.js";
 
 const ProductRoutes = express.Router()
 
 ProductRoutes.post('/create/:userId' , ProductCreate )
-ProductRoutes.put('/update/:id' , Update)
-
+ProductRoutes.put('/update/:id' , UpdateProduct)
+ProductRoutes.delete('/delete/:id' , DeleteProduct )
+ProductRoutes.get('/getProducts/:userId' , GetProducts ) 
 
 
 
