d72bb6effa19d610109dd3b117d2c6d1096d36d1
Ali Mousazada <mousazada.ali@gmail.com>
2025-01-19 14:04:03 -0500

    create hero region top villa

--- RealStateWeb/client/src/components/Footer/Footer.jsx
+++ RealStateWeb/client/src/components/Footer/Footer.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Link , useLocation } from "react-router-dom";
+
+
+
+
 
 const Footer = () => {
+
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
+
+
+
   return (
     <>
-      
+      <footer className={
+         isHomePage ? 'homePage_footer otherPage_footer': "otherPage_footer"
+        }>
+        <div className="container">
+          <h4>LUXURY RENTALS</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum et
+            minus voluptatibus corporis eligendi quasi iure nihil eum reiciendis
+            quod.
+          </p>
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/termsandconditions"}>Terms&Conditions</Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="container">
+          <h4>Connect with us</h4>
+          <p>+00 000 000 00</p>
+          <p>Amou5255zada@gmail.com</p>
+          <p>© All Rights Reserved By abcdefgh.</p>
+        </div>
+      </footer>
     </>
   )
 }
--- RealStateWeb/client/src/components/Home/Home.jsx
+++ RealStateWeb/client/src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import HeroSection from '../../subComponents/HeroSection'
+import TopVillas from '../../subComponents/TopVillas'
+import Regions from '../../subComponents/Regions'
+import OurSpecialities from '../../subComponents/OurSpecialities'
 
 const Home = () => {
   return (
     <>
-      <h1>this is home page</h1>
+    <article className='page'>
+        <HeroSection />
+        <TopVillas />
+        <Regions />
+        <OurSpecialities />
+    </article>
     </>
   )
 }
