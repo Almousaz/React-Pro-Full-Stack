af94ffbe39ff2a72d8483a948ea0315ecc095199
Ali Mousazada <mousazada.ali@gmail.com>
2025-01-24 23:58:10 -0500

    collection and hero

--- FashionEcommerceWeb/my-project/src/sections/Hero.jsx
+++ FashionEcommerceWeb/my-project/src/sections/Hero.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import banner4 from "../assets/banner4.jpg";
+import { useDarkMode } from "../components/DarkModeContext";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
-  return (
-    <div>
-      <h2>hero</h2>
-    </div>
-  )
-}
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
 
-export default Hero
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <>
+      <section
+        id="hero"
+        className={`${
+          darkMode ? "dark bg-[#19191a]" : "light bg-slate-300"
+        } w-full lg:px-20 px-10 lg:py-20 py-8 lg:h-screen h-fit
+        dark:bg-slate-700 dark:bg-transparent flex flex-col lg:flex-row justify-between
+        items-center bg-cover bg-center gap-10`}
+      >
+        <div className=" flex flex-col justify-center items-start gap-6 lg:w-[60%] w-full">
+          <h1
+            data-aos="zoom-in"
+            className="text-3xl text-slate-900 italic dark:text-white"
+          >
+            Chick Charismas
+          </h1>
+          <h1
+            data-aos="zoom-in"
+            className="lg:text-7xl text-6xl text-black dark:text-white font-bold capitalize"
+          >
+            Effortless <br></br> glamour for <br />
+            every occassions
+          </h1>
+          <button
+            data-aos="zoom-in"
+            className="bg-black px-10 py-4 font-semibold text-white text-lg rounded-lg mt-8 hover:bg-white dark:text-black hover:text-black cursor-pointer"
+          >
+            Shop Now
+          </button>
+        </div>
+
+        <div id="image-box" className="lg:w-[40%] w-full">
+          <img
+            data-aos="zoom-in"
+            src={banner4}
+            alt="hero image"
+            className="bg-cover bg-center"
+          />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
